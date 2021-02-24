754f1545ba448f9dd5044f86dd4e0bdf43668469
Abdi~ <kaliq.i@outlook.com>
2021-02-24 12:02:15 +0000

    comments

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,15 @@ function App() {
   const [inputSearch, setInputSearch] = useState("")
   const [favourites, setFavourites] = useState<any>([])
   
+  //currently rewriting the add or remove function: my thoughts
+  // Have initialData in the state
+  // then create variable called filteredarray which filters initialdata for elements whose property DO NOT match paramenter
+  // then run if statement with condition that length of filtered array is different from initialdata
+  
+  // but envisioned problems = if initial data is in state - how do we initialise this without every render starting with all the original json data
+  // filtered array will always be different initialdata if initialdata is beng initialised from json in component
+  ///more thinking
+
   const addOrRemove = (id:number) => {
     // if it exists in initial data - remove from it and add to favourites
     const arrayWithSearched = initialData.filter((e)=>e.id === id)
