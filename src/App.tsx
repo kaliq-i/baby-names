821fc8f9c8b8228038a964af6fa5e155451d124f
Abdi~ <kaliq.i@outlook.com>
2021-02-24 10:25:20 +0000

    css

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
+
 import babyNamesData from "./babyNamesData.json"
 import Child from "./Component/child"
 
 function App() {
 
-const nameMapper = ({name}:{name:string}) => {
-  return (
-    <div>{name}</div>>
-  )
-} 
-let mappedNames = babyNamesData.map(nameMapper)  
+  const [inputSearch, setInputSearch] = useState("")
+  const [favourites, setFavourites] = useState([""])
 
+  const nameMapper = ({ name, sex }: { name: string, sex: string }) => {
+    return (
+      <div className={`holder ${sex === "m" ? "male" : "female"}`}>{name}</div>
+    )
+  }
+
+  let sortedNames = babyNamesData.sort((a, b) => a.name.localeCompare(b.name))
+  let filteredNames = sortedNames.filter((item) => {
+    return (`${item.name.toLowerCase()}`).startsWith(`${inputSearch.toLowerCase()}`)
+  })
+  let mappedNames = filteredNames.map(nameMapper)
   return (
     <div >
-     {mappedNames}
+      
+      <div>
+        <input value={inputSearch} onChange={(event) => setInputSearch(event.target.value)} />
+      </div>
+      <div>
+        Favourites List: 
+        <hr />
+        {favourites}
+      </div>
+
+      Normal List:
+      <hr/>
+      <div>
+        {mappedNames}
+      </div>
+
     </div>
   );
 }
