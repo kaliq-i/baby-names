d94cabc5eee6d1288ab94dd5c9d2f3ff80515748
Abdi~ <kaliq.i@outlook.com>
2021-02-24 14:11:28 +0000

    baby names error

--- src/App.tsx
+++ src/App.tsx
@@ -8,21 +8,22 @@ const initialData = babyNamesData
 function App() {
 
   const [inputSearch, setInputSearch] = useState("")
+  const [radio,setRadio] = useState("all")
   const [favourites, setFavourites] = useState<any>([])
-  
+
   //currently rewriting the add or remove function: my thoughts
   // Have initialData in the state
   // then create variable called filteredarray which filters initialdata for elements whose property DO NOT match paramenter
   // then run if statement with condition that length of filtered array is different from initialdata
-  
+
   // but envisioned problems = if initial data is in state - how do we initialise this without every render starting with all the original json data
   // filtered array will always be different initialdata if initialdata is beng initialised from json in component
   ///more thinking
 
-  const addOrRemove = (id:number) => {
+  const addOrRemove = (id: number) => {
     // if it exists in initial data - remove from it and add to favourites
-    const arrayWithSearched = initialData.filter((e)=>e.id === id)
-    const arrayWithSearchedFavourite = favourites.filter((e:any) =>e.id === id )
+    const arrayWithSearched = initialData.filter((e) => e.id === id)
+    const arrayWithSearchedFavourite = favourites.filter((e: any) => e.id === id)
     if (arrayWithSearched.length > 0) {
       initialData.splice(initialData.findIndex(item => item.id === id), 1)
       const newFavourites = [...favourites]
@@ -38,9 +39,9 @@ function App() {
       setFavourites([...newFavourites])
 
     }
-    
+
   }
-  const nameMapper = ({ name, sex, id }: { name: string, sex: string, id:number }) => {
+  const nameMapper = ({ name, sex, id }: { name: string, sex: string, id: number }) => {
     return (
       <button className={`holder ${sex === "m" ? "male" : "female"}`} onClick={() => addOrRemove(id)}>{name}</button>
     )
@@ -48,25 +49,33 @@ function App() {
 
   let sortedNames = initialData.sort((a, b) => a.name.localeCompare(b.name))
   let filteredNames = sortedNames.filter((item) => {
-    return (`${item.name.toLowerCase()}`).startsWith(`${inputSearch.toLowerCase()}`)
+    return (item.name.toLowerCase().startsWith(inputSearch.toLowerCase()) && radio!== "all"? item.sex === radio: item.sex === "m"||"f"  ) //why doesnt this work - input doesnt filter
   })
   let mappedNames = filteredNames.map(nameMapper)
   let mappedFavourites = favourites.map(nameMapper)
- 
   return (
     <div >
-      
+
       <div>
-        <input value={inputSearch} onChange={(event) => setInputSearch(event.target.value)} />
+        <input value={inputSearch} onChange={(event) => setInputSearch(event.target.value)} /><br />
+        <div>
+          <input type="radio" id="male" name="gender" value="m" onChange={(event) => setRadio(event.target.value)}/>
+          <label htmlFor="male">male</label><br />
+          <input type="radio" id="female" name="gender" value="f" onChange={(event) => setRadio(event.target.value)} />
+          <label htmlFor="female">female</label><br />
+          <input type="radio" id="all" name="gender" value="all" onChange={(event) => setRadio(event.target.value)}/>
+          <label htmlFor="all">all</label><br />
+        </div>
       </div>
+
       <div>
-        Favourites List: 
+        Favourites List:
         <hr />
         {mappedFavourites}
       </div>
 
       Normal List:
-      <hr/>
+      <hr />
       <div>
         {mappedNames}
       </div>
